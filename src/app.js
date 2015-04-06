b5ed44e120bb0b649d106ee7d70d338d43557e49
Дмитрий Пацура <talk@dmtry.me>
2015-04-06 11:52:50 +0900

    Header menu - active link highlight

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 'use strict';
 
+angular.module('link', []).
+    directive('activeLink', ['$location', function(location) {
+        return {
+            restrict: 'A',
+            link: function($scope, element, attrs, controller) {
+                var path = attrs.href;
+                var clazz = attrs.activeLink;
+
+                $scope.location = location;
+                $scope.$watch('location.path()', function(newPath) {
+                    if (path === newPath) {
+                        element.addClass(clazz);
+                    } else {
+                        element.removeClass(clazz);
+                    }
+                });
+            }
+        };
+    }]);
+
 var app = angular.module('app', [
     'ngRoute',
-    'ngResource'
+    'ngResource',
+    'link'
 ]);
 
 hljs.initHighlightingOnLoad();
